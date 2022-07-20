d13097f4eb271e6dad4966203062bc2a320f1bdc
tonyfilho <oficinadosdrones@gmail.com>
2022-07-20 10:51:53 +0100

    start training-AlgaWorks

---
+++ src/main/java/com/tony/clientes/trainingAlgaWorks/TrainingAlgaWorksApplication.java
@@ -0,0 +1,13 @@
+package com.tony.clientes.trainingAlgaWorks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingAlgaWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingAlgaWorksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tony/clientes/trainingAlgaWorks/TrainingAlgaWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tony.clientes.trainingAlgaWorks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingAlgaWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
